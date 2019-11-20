5db87c7a9b2ba4d3da7bf804c4074838bec9fbf7
Jahir Medina <jahir.medina@pm.me>
2019-11-20 00:18:24 -0500

    v0.00.00.3000

--- _posts/2019-11-15-Modelo-Jerarquico.md
+++ _posts/2019-11-15-Modelo-Jerarquico.md
@@ -11,9 +11,12 @@ file_name: "2019-11-15-Modelo-Jerarquico"
 # alt_js_inits:
 #  - webgl_scene2
 additional_scripts:
-#  - "https://cdn.jsdelivr.net/npm/webgl-operate@latest/dist/webgl-operate.min.js"
 #  - "https://cdnjs.cloudflare.com/ajax/libs/mathjs/6.2.2/math.min.js"
-  - "https://cdnjs.cloudflare.com/ajax/libs/three.js/r83/three.min.js"
+    - "https://cdnjs.cloudflare.com/ajax/libs/three.js/109/three.min.js"
+additional_modules:
+    - "https://cdnjs.cloudflare.com/ajax/libs/tween.js/18.3.1/Tween.min.js"
+    - "https://cdnjs.cloudflare.com/ajax/libs/stats.js/r16/Stats.min.js"
+special_init: "threejs"
 ---
 
 ___Lorem Ipsus :D___
--- assets/post_assets/2019-11-15-Modelo-Jerarquico/myscript.js
+++ assets/post_assets/2019-11-15-Modelo-Jerarquico/myscript.js
@@ -1,53 +1,102 @@
 function webgl_scene(POST_NAME){
-
-  var RETURN_PATH = "../../../assets/post_assets/"
-  var REF_ASSETS_PATH = RETURN_PATH + POST_NAME
-  var JSON_OBJ_PATH = REF_ASSETS_PATH + "/OBJ_files/obj.JSON"
-
+  let REF_ASSETS_PATH = "../../../assets/post_assets/" + POST_NAME + "/";
+  let JSON_OBJ_PATH = REF_ASSETS_PATH + "/OBJ_files/obj.JSON"
   let canvas = document.querySelector('#glcanvas');
-  console.log(canvas.clientWidth, canvas.clientHeight);
-  // Create an empty scene
-  var scene = new THREE.Scene();
 
-  
-  var camera = new THREE.PerspectiveCamera( 90, canvas.clientWidth/canvas.clientHeight, 0.1, 1000 );
-  camera.position.z = 4;
+  let perspectiveCamera, controls, scene, renderer, stats;
 
-  // Create a renderer with Antialiasing
-  var renderer = new THREE.WebGLRenderer({antialias:true, canvas: glcanvas});
 
-  // Configure renderer clear color
-  renderer.setClearColor("#000000");
+  init();
+  animate();
 
-  // Configure renderer size
-  renderer.setSize( canvas.clientWidth, canvas.clientHeight, false);
+  function init() {
+    scene = new THREE.Scene();
 
-  // Append Renderer to DOM
-  // console.log(renderer.domElement);
-  // document.querySelector('#glcanvas').appendChild( renderer.domElement );
+    camera = new THREE.PerspectiveCamera( 75, canvas.clientWidth/canvas.clientHeight, 1, 20000 );
+    camera.position.set(0, 100, 200);
+    camera.lookAt(scene.position);
 
-  // ------------------------------------------------
-  // FUN STARTS HERE
-  // ------------------------------------------------
+    renderer = new THREE.WebGLRenderer({antialias:true, canvas: glcanvas});
+    renderer.setSize( canvas.clientWidth, canvas.clientHeight, false);
+    renderer.setClearColor("#000000");
 
-  // Create a Cube Mesh with basic material
-  var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  var material = new THREE.MeshBasicMaterial( { color: "#433F81" } );
-  var cube = new THREE.Mesh( geometry, material );
+    //SCENE
 
-  // Add cube to Scene
-  scene.add( cube );
+    let axes = new THREE.AxesHelper(100);
+    scene.add(axes);
 
-  // Render Loop
-  var render = function () {
-    requestAnimationFrame( render );
+    floor();
+    skyBox();
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
 
-    // Render the scene
+    // objects
+
+    let material = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load(REF_ASSETS_PATH + 'textures/crate.jpg') });
+    let geometry = new THREE.CubeGeometry(50, 50, 50);
+    let cube = new THREE.Mesh(geometry, material);
+    cube.position.set(0, 25, 0);
+    scene.add(cube);
+
+    // controlls
+
+    createControls( perspectiveCamera );
+
+    // stats
+
+    stats = new Stats();
+    canvas.appendChild( stats.dom );
+  }
+
+  function animate() {
+    requestAnimationFrame( animate );
+    render();
+  }
+
+  function render() {
+    let camera = perspectiveCamera;
     renderer.render(scene, camera);
-  };
+  }
 
-  render();
+    //////////////////////////// Functions
+
+
+  function skyBox() {
+    let materialArray = [];
+    materialArray.push(new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load( REF_ASSETS_PATH + 'textures/dawnmountain-xpos.png' ) }));
+    materialArray.push(new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load( REF_ASSETS_PATH + 'textures/dawnmountain-xneg.png' ) }));
+    materialArray.push(new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load( REF_ASSETS_PATH + 'textures/dawnmountain-ypos.png' ) }));
+    materialArray.push(new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load( REF_ASSETS_PATH + 'textures/dawnmountain-yneg.png' ) }));
+    materialArray.push(new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load( REF_ASSETS_PATH + 'textures/dawnmountain-zpos.png' ) }));
+    materialArray.push(new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load( REF_ASSETS_PATH + 'textures/dawnmountain-zneg.png' ) }));
+    for (let i = 0; i < 6; i++)
+        materialArray[i].side = THREE.BackSide;
+    let skyboxMaterial = materialArray;
+    let skyboxGeom = new THREE.CubeGeometry( 5000, 5000, 5000, 1, 1, 1 );
+    let skybox = new THREE.Mesh( skyboxGeom, skyboxMaterial );
+    scene.add( skybox );
+  }
+
+  function floor() {
+    let floorTexture = new new THREE.TextureLoader().load( REF_ASSETS_PATH + 'textures/checkerboard.jpg' );
+    floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
+    floorTexture.repeat.set( 10, 10 );
+    let floorMaterial = new THREE.MeshBasicMaterial({ map: floorTexture, side: THREE.DoubleSide });
+    let floorGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10);
+    let floor = new THREE.Mesh(floorGeometry, floorMaterial);
+    floor.position.y = -0.5;
+    floor.rotation.x = Math.PI / 2;
+    scene.add(floor);
+  }
+
+  function createControls( camera ) {
+    controls = new TrackballControls( camera, renderer.domElement );
+    controls.rotateSpeed = 1.0;
+    controls.zoomSpeed = 1.2;
+    controls.panSpeed = 0.8;
+    controls.staticMoving = true;
+    controls.dynamicDampingFactor = 0.3;
+    controls.keys = [ 65, 83, 68 ];
+    controls.addEventListener( 'change', render );
+  }
 }
+  
